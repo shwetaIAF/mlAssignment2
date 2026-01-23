d7a67caa8bb1557bc956fd65bc9b1404e0300eea
Shweta Gaur <56163605+shwetaIAF@users.noreply.github.com>
2026-01-23 16:02:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if not os.path.exists(MODEL_FILE):
     st.info("First run: Training models... please wait ⏳ (~40 sec)")
 
     
-    df = pd.read_csv(data/test.csv)
+    df = pd.read_csv(test.csv)
 
     X = df.drop("Activity", axis=1)
     y = df["Activity"]
@@ -102,5 +102,6 @@ if uploaded_file:
             results[name] = preds_labels[:5]
 
         st.write(pd.DataFrame(results))
+
 
 
